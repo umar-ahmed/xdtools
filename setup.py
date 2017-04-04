50889ac94823e4cf135e7a928e7ad0aa1c71ede6
umar-ahmed <umar.ahmed1998@gmail.com>
2017-04-04 19:29:02 -0400

    Added file structure for package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'xd-tools',
+    packages = ['xd-tools'],
+    version = '0.0.0',
+    description = 'An unofficial Python API for Adobe Experience Design',
+    author = 'Umar Ahmed',
+    author_email = 'umar.ahmed@mail.utoronto.ca',
+    url = 'https://github.com/umar-ahmed/xd-tools',
+    download_url = '', # TODO: Create release and put release link here
+    keywords = ['adobe-xd', 'python', 'file-parser', 'api'],
+    classifiers = [],
+)
