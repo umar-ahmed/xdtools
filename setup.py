018df8bc71e1150e606f73e602ee70aa8fabfcbe
Umar Ahmed <umar.ahmed1998@gmail.com>
2017-12-28 22:34:20 -0500

    Change version number.

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup
 setup(
     name='xdtools',
     packages=['xdtools'],
-    version='0.1',
+    version='0.0.1',
     description='An unofficial Python API for Adobe Experience Design',
     author='Umar Ahmed',
     author_email='umar.ahmed@mail.utoronto.ca',
     url='https://github.com/umar-ahmed/xdtools',
-    download_url='https://github.com/umar-ahmed/xdtools/archive/0.1.tar.gz',
+    download_url='https://github.com/umar-ahmed/xdtools/archive/0.0.1.tar.gz',
     keywords=['adobe', 'xd', 'ui', 'ux'],
     license='MIT',
     zip_safe=False
