f0875e0afcd244bf593c93a77dca8ffe11f2c839
Umar Ahmed <umar.ahmed1998@gmail.com>
2017-12-28 20:59:09 -0500

    Update README and files for PyPI.

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 **This is currently a WIP.** XD Tools is an unofficial Python API for Adobe XD. It provides the ability to parse and read data from `.xd` files that are created by Adobe XD using Python 3. As Adobe XD is constantly changing each month with new features, this API will inevitably change in order to stay up-to-date, so please report issues on GitHub if you find any.
 
+## Installation
+
+```
+pip install xdtools
+```
+
 ## Planned Features
 
 ### Read data from an XD file
--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ from setuptools import setup
 
 setup(
     name='xdtools',
+    packages=['xdtools'],
     version='0.1',
     description='An unofficial Python API for Adobe Experience Design',
-    url='https://github.com/umar-ahmed/xdtools',
     author='Umar Ahmed',
     author_email='umar.ahmed@mail.utoronto.ca',
+    url='https://github.com/umar-ahmed/xdtools',
+    download_url='',
+    keywords=['adobe', 'xd', 'ui', 'ux'],
     license='MIT',
-    packages=['xdtools'],
     zip_safe=False
 )
